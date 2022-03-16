b8d89d204c9421be04445d19df0dbd3b80b7e646
Kora Santiago <91646955+koko-santi@users.noreply.github.com>
2022-03-16 21:04:08 +0000

    updates code

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var data = require('./data/test.json');
+
+app.set('view engine','ejs');
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = 'My Home Page';
+  res.render('pages/index',{'title':title});
+});
+
+
+app.get('/about-us', (req, res) => {
+  var title = 'My About Us Page';
+  res.render('pages/about-us',{'title':title});
+});
+
+
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+}); 
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(data);
+});
